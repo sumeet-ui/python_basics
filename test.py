61a649a27399e6652facecc49fa8b002a708ff88
sumeet <4al23ai057@aiet.org>
2026-06-25 16:57:24 +0530

    git commonds

---
+++ test.py
@@ -0,0 +1,29 @@
+print("Hello World!",end= "")
+print("Hello World!",end= "")
+print("Hello World!")
+
+print("Hello World!","word",sep= "#")
+#print("Hello World!",end= "")
+#print("Hello World!")
+print("a">"b")
+
+x="10.5"
+print(int(float(x)))
+
+print("\\test\\ntesting")
+print("\test\ntesting")
+print(r"\test\ntesting")
+
+x1=("hello")
+print("""hello)
+      my name is 
+      gobi""")
+
+
+print(x1[0:2:3])
+
+print(x1[::-1])
+
+while True:
+    print("hello")
+    brea
